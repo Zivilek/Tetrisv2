8b3168897e4ec297a783564f29c34f715c5965ab
Zivile <zkinduryte@gmail.com>
2016-11-19 13:41:18 +0200

    bl;a

--- Assets/scripts/Figures.cs
+++ Assets/scripts/Figures.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Figures : MonoBehaviour {
 
+    float fall = 0;
+    public float fallSpeed = 1;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -19,18 +22,42 @@ public class Figures : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.position += new Vector3(1, 0, 0);
+
+            if (checkIsValidPosition())
+            {
+
+            }
+            else
+            {
+                transform.position += new Vector3(-1, 0, 0);
+            }
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(-1, 0, 0);
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed)
         {
             transform.position += new Vector3(0, -1, 0);
+            fall = Time.time;
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             transform.Rotate(0, 0, 90);
         }
+    }
+
+    bool checkIsValidPosition()
+    {
+        foreach (Transform mino in transform)
+        {
+            Vector2 position = FindObjectOfType<Game>().Round(mino.position);
+
+            if (FindObjectOfType<Game>().checkIfInsideGrid(position) == false)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
---
+++ Assets/scripts/game.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+    public int gridWidth = 11;
+    public int gridHeight = 18;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public bool checkIfInsideGrid(Vector2 position)
+    {
+        return ((int)position.x >= -6 && (int)position.x < 6 && (int)position.y >= -9);
+    }
+
+    public Vector2 Round (Vector2 position)
+    {
+        return new Vector2(Mathf.Round(position.x), Mathf.Round(position.y));
+    }
+}
